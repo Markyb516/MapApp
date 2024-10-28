51909aa37f5423796b30283d409ae78030a8e42c
Markus <Markus.buchanan1125@gmail.com>
2024-10-27 21:44:28 -0400

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Markus Buchanan on 10/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapAppTests/MapAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppTests.swift
+//  MapAppTests
+//
+//  Created by Markus Buchanan on 10/27/24.
+//
+
+import Testing
+@testable import MapApp
+
+struct MapAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
